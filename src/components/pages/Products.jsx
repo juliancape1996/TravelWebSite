1abc3f5d72cc76aeec3dd14d42126b11a6c38156
Julian Calle Perez <juliancp17@gmail.com>
2024-01-20 14:59:03 -0500

    TravelWebSite

---
+++ src/components/pages/Products.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../App.css'
+
+export default function Products() {
+  return (
+    <>
+        <h1 className='products'>
+            PRUDUCTS
+        </h1>
+    </>
+  )
+}
